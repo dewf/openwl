db97f720c5c7e7c9ccf547b0968ba213fb08785e
Daniel G <dewf@users.noreply.github.com>
2019-04-26 12:10:55 -0600

    release testing (#13)
    deploying to bintray at the moment (didn't have luck trying to create a single github release from two CI providers)

--- ci/travis/gen_bintray.py
+++ ci/travis/gen_bintray.py
@@ -2,25 +2,25 @@ import sys
 import json
 
 if len(sys.argv) != 2:
-    print("usage: gen_bintray.py <build-ID>")
+    print("usage: gen_bintray.py <build-tag>")
     sys.exit(-1)
 
-build_id = sys.argv[1]
+build_tag = sys.argv[1]
 
 out_desc = {
     "package": {
-        "name": "MyPackage",
-        "repo": "OpenWL",
+        "name": "OpenWL",
+        "repo": "TeamGUI",
         "subject": "dewf",
         "vcs_url": "https://github.com/dewf/openwl.git",
         "licenses": ["MIT"],
-        "labels": ["cool", "awesome", "gorilla"],
+        # "labels": ["cool", "awesome", "gorilla"],
         "public_download_numbers": True,
         "public_stats": True
     },
 
     "version": {
-        "name": "OpenWL-%s" % (build_id,),
+        "name": build_tag, #"OpenWL-%s" % (build_tag,),
         "desc": "OpenWL Mac CI Build",
         #"released": "2015-01-04",
         #"vcs_tag": "0.5",
@@ -31,7 +31,7 @@ out_desc = {
         [
             {
                 "includePattern": "OpenWL.dmg", 
-                "uploadPattern": "travis-builds/%06d/OpenWL.dmg" % (int(build_id),),
+                "uploadPattern": "OpenWL/%s/OpenWL-%s-Mac-Framework.dmg" % (build_tag, build_tag),
                 "matrixParams":
                 { 
                     "override": 1 
