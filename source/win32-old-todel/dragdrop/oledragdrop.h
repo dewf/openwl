fea40e65504a7df95d13ac00ca4864b7f3cf9c9f
Daniel X <nobody@nowhere.net>
2022-06-04 19:02:09 -0600

    removed old win32 stuff finally

--- source/win32-old-todel/dragdrop/oledragdrop.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void dragDropInit();
-void dragDropShutdown();
-void dragDropRegisterTarget(HWND hwnd);
-void dragDropUnregisterTarget(HWND hwnd);
-
--- source/win32-old-todel/wndproc.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
-LRESULT CALLBACK topLevelWindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
